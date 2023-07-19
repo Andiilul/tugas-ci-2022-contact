2f8af19df3aabe456f9e472f15322e763d04f981
Andiilul <hafidamfa21d@student.unhas.ac.id>
2023-07-19 17:45:46 +0800

    add  Contactbox.jsx

---
+++ src/components/organism/ContactBox.jsx
@@ -0,0 +1,13 @@
+import Card from "../molecules/Card"
+
+function Contactbox() {
+  return (
+    <main className="flex h-screen p-8 bg-secondary">
+      <div className="bg-primary w-full rounded-md items-center justify-center"></div>
+      <Card/>
+      dfdfdf  
+    </main>
+  )
+}
+
+export default Contactbox
