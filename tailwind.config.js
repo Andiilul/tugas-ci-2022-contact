b20858fea7f660144cd755422ed19b26257857e2
Andiilul <hafidamfa21d@student.unhas.ac.id>
2023-07-19 17:44:28 +0800

    add animation and color in tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,28 @@ export default {
   ],
   theme: {
     extend: {
+      animation:{
+        'topToBot':'TopToBot 300ms ease-out',
+        'botToTop':'BotToTop 400ms ease-in',
+
+      },
+      keyframes:{
+        'TopToBot':{
+          '0%':{transform:'translateY(-100%)'},
+          '100%':{transform:'translateY(0)'},
+        },
+        'BotToTop':{
+          '0%':{transform:'translateY(0)', position:'fixed'},
+          '70%':{transform:'translateY(-100%);'},
+          '100%':{position:'absolute'}
+        },
+      },
+      colors:{  
+        primary:"#1E1E1E",
+        secondary:"#EBB701",
+
+        
+      }
     },
   },
   plugins: [],
