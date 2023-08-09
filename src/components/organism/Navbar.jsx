2e59ff30b3a6a98a96fe1bca79b436309ae5e36f
Andiilul <hafidamfa21d@student.unhas.ac.id>
2023-08-09 22:18:03 +0800

    Edit Navbar

--- src/components/organism/Navbar.jsx
+++ src/components/organism/Navbar.jsx
@@ -1,53 +1,69 @@
-import { useState, useEffect } from 'react';
-import Navitems from '../atoms/Navitems';
+import { useState, useEffect } from "react";
+import Navitems from "../atoms/Navitems";
+import reactLogo from "../../assets/react.svg"
 
 export default function Navbar() {
-  const navItemList = [
+	const navItemList = [
+		{
+			name: "Home",
+			href: "/",
+      title:""
+		},
     {
-      title: 'Home',
-      href: '/',
+      name: "Features",
+      href: "/",
+      title:""
     },
-    {
-      title: 'Contacts',
-      href: '/',
-    },
-    {
-      title: 'Features',
-      href: '/',
-    },
-    {
-      title: 'About Us',
-      href: '/',
-    },
-  ];
+		{
+			name: "Contacts",
+			href: "/",
+      title:""
+		},
+		{
+			name: "About",
+			href: "https://github.com/Andiilul",
+      title:"https://github.com/Andiilul"
+		},
+	];
 
-  const [isScrolled, setIsScrolled] = useState(false);
+	const [isScrolled, setIsScrolled] = useState(false);
 
-  useEffect(() => {
-    const handleScroll = () => {
-      const scrollY = window.scrollY;
-      console.log(scrollY)
-      setIsScrolled(scrollY >= 200);
-    };
+	useEffect(() => {
+		const handleScroll = () => {
+			const scrollY = window.scrollY;
+			console.log(scrollY);
+			setIsScrolled(scrollY >= 200);
+		};
 
-    window.addEventListener('scroll', handleScroll);
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
+		window.addEventListener("scroll", handleScroll);
+		return () => {
+			window.removeEventListener("scroll", handleScroll);
+		};
+	}, []);
 
-  const Transition = isScrolled ? 'bg-[rgba(3,4,94,0.8)] animate-topToBot fixed text-white  ' : ' absolute text-antiwhite';
+	const Transition = isScrolled
+		? "bg-[rgba(38,38,38,0.9)] animate-topToBot fixed text-white bg-dark shadow-lg "
+		: "bg-transparent absolute text-antiwhite animate-botToTop";
 
-  return (
-    <nav
-      className={`${Transition} bg-indigo  top-0 w-full shadow-sm p-[16px_64px] backdrop-blur-[2px] backdrop-invert flex items-center justify-between`}
-    >
-      <section className="text-[32px] font-light font-[Philosopher]">ContactBooks</section>
-      <section className="flex gap-8">
-        {navItemList.map((item, index) => (
-          <Navitems key={index} scrolled={isScrolled} name={item.title} navTo={item.href} />
-        ))}
-      </section>
-    </nav>
-  );
+	return (
+		<nav
+			className={`${Transition} z-[100] top-0 w-full  p-[16px_64px] backdrop-blur-[6px]  flex items-center justify-between `}
+		>
+			<section className="text-[32px] font-light select-none flex gap-[10px]">
+				<p className="font-[Philosopher] ">ReactContact</p>
+        <img draggable={false} src={reactLogo}></img>
+			</section>
+			<section className="flex gap-8 ">
+				{navItemList.map((item, index) => (
+					<Navitems
+						key={index}
+						scrolled={isScrolled}
+						name={item.name}
+						navTo={item.href}
+            title={item.title}
+					/>
+				))}
+			</section>
+		</nav>
+	);
 }
