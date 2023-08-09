d255c484ac4e63876c5389d8cc01cc0de89a7b6e
Andiilul <hafidamfa21d@student.unhas.ac.id>
2023-08-09 22:18:18 +0800

    add title on each navitems

--- src/components/atoms/NavItems.jsx
+++ src/components/atoms/NavItems.jsx
@@ -4,21 +4,22 @@ Navitems.propTypes = {
 	name: PropTypes.string.isRequired,
 	navTo: PropTypes.string.isRequired,
 	scrolled: PropTypes.bool.isRequired,
+	title:PropTypes.string
 };
 
 export default function Navitems(props) {
-	const { name, navTo, scrolled} = props;
+	const { name, navTo, scrolled,title} = props;
 
-	const hoverColor = scrolled? 'hover:text-oranges after:bg-oranges':'hover:text-white after:bg-white'
+	const hoverColor = scrolled? 'hover:text-oranges after:bg-oranges':'hover:text-cyan after:bg-cyan'
 
 
 
 	return (
-		<button
+		<a
 			className={`${hoverColor} cursor-pointer font-[400] flex items-center justify-center p-2 flex-col after:content-[""] duration-300 after:rounded-lg after:-mt-1 after:w-0 after:h-[2px]  after:duration-300 hover:after:w-full hover:after:duration-300 text-[18px] `}
-			href={navTo}
+			href={navTo} title={title}
 		>
 			{name}
-		</button>
+		</a>
 	);
 }
